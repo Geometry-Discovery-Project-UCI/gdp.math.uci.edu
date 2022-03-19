3ad7266b2fcd6576471eb1e18dcba292c0109759
Kiyoshi <kiyo5hi@riseup.net>
2022-03-19 15:55:21 +0800

    chore(index): fixed a typo (#15)

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
                 topics: [
                     new Topic("Morley's Miracle", ["Zhiqin Lu"], "public/images/topic01.png", "topics/topic01"),
                     new Topic("Ceva's and Menelaus' Theorems", ["Zhiqin Lu", "Xianfu Liu"], "public/images/topic02.png", "topics/topic02"),
-                    new Topic("Five Triangle Cneters", ["Zhiqin Lu", "Shiyi Lyu"], "public/images/topic03.png", "topics/topic03"),
+                    new Topic("Five Triangle Centers", ["Zhiqin Lu", "Shiyi Lyu"], "public/images/topic03.png", "topics/topic03"),
                 ]
             }
         },
