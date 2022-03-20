a97996d60c54ef8364643dd5c2463d6c48c265eb
Kiyoshi Guo <kiyo5hi@riseup.net>
2022-03-20 12:12:17 +0800

    fix(*): formatting and conventions

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 {
-    const innerContent = document.getElementById("inner-content");
+    const main = document.getElementById("main-content");
     class Topic {
         constructor(title, authors, imageUrl, pageUrl) {
             this.title = title;
@@ -18,9 +18,9 @@
                     new Topic("Five Triangle Centers", ["Zhiqin Lu", "Shiyi Lyu", "Xinxi Guo"], "public/images/topic03.png", "topics/topic03"),
                     new Topic("Erdős-Mordell Inequality", ["Zhiqin Lu"], "public/images/topic04.png", "topics/topic04"),
                     new Topic("Simson Line", ["Zhiqin Lu"], "public/images/topic05.png", "topics/topic05"),
-                    new Topic("Pascal and Brainchon Theorems", [""], "public/images/topic06.png", "topics/topic06"),
+                    new Topic("Pascal and Brainchon Theorems", ["Zhiqin Lu", "Xianfu Liu"], "public/images/topic06.png", "topics/topic06"),
                     new Topic("Isogonal Conjugate and Isotomic Conjugate Points", ["Zhiqin Lu", "Shiyi Lyu"], "public/images/topic07.png", "topics/topic07"),
-                    new Topic("Gergonne and Nagel Points", [""], "public/images/topic08.png", "topics/topic08"),
+                    new Topic("Gergonne and Nagel Points", [], "public/images/topic08.png", "topics/topic08"),
                     new Topic("Napoleon's Theorem", ["Zhiqin Lu"], "public/images/topic09.png", "topics/topic09"),
                     new Topic("Ptolemy Theorem's and Kelvin Transform", ["Zhiqin Lu"], "public/images/topic10.png", "topics/topic10"),
                     new Topic("Dual Triangles", ["Zhiqin Lu"], "public/images/topic11.png", "topics/topic11"),
@@ -29,8 +29,8 @@
             }
         },
         methods: {
-            reloadInnerContent(topic) {
-                innerContent.src = topic.pageUrl;
+            reloadMainContent(topic) {
+                main.src = topic.pageUrl;
                 window.scrollTo({
                     top: 0,
                     behavior: 'smooth'
