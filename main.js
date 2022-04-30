fb5c190c88b81a34c5c2a0b0473fd6fb015ab818
Kiyoshi <kiyo5hi@riseup.net>
2022-04-30 13:59:26 +0800

    chore(*): seperate animation contribs from essay contribs (#70)

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 {
   class Topic {
-    constructor(title, authors, imageUrl, pageUrl, scripts) {
+    constructor(
+      title,
+      essayContributors,
+      animationContributors,
+      imageUrl,
+      pageUrl,
+      scripts
+    ) {
       this.title = title;
-      this.authors = authors;
+      this.essayContributors = essayContributors;
+      this.animationContributors = animationContributors;
       this.imageUrl = imageUrl;
       this.pageUrl = pageUrl;
       this.scripts = scripts;
@@ -16,6 +24,7 @@
     new Topic(
       "Morley's Miracle",
       ["Zhiqin Lu"],
+      ["Zhiqin Lu", "Melissa Yu"],
       "public/images/topic01.png",
       "topics/topic01",
       ["morley.js"]
@@ -23,12 +32,14 @@
     new Topic(
       "Ceva's and Menelaus' Theorems",
       ["Zhiqin Lu", "Xianfu Liu"],
+      [],
       "public/images/topic02.png",
       "topics/topic02"
     ),
     new Topic(
       "Five Triangle Centers",
-      ["Zhiqin Lu", "Shiyi Lyu", "Xinxi Guo"],
+      ["Zhiqin Lu", "Shiyi Lyu"],
+      ["Xinxi Guo"],
       "public/images/topic03.png",
       "topics/topic03",
       ["centroid.js", "incenter.js", "orthocenter.js"]
@@ -36,12 +47,14 @@
     new Topic(
       "Erdős-Mordell Inequality",
       ["Zhiqin Lu"],
+      [],
       "public/images/topic04.png",
       "topics/topic04"
     ),
     new Topic(
       "Simson Line",
       ["Zhiqin Lu", "Shiyi Lyu"],
+      ["Melissa Yu"],
       "public/images/topic05.png",
       "topics/topic05",
       ["simsonline.js"]
@@ -49,12 +62,14 @@
     new Topic(
       "Pascal and Brainchon Theorems",
       ["Zhiqin Lu", "Xianfu Liu"],
+      [],
       "public/images/topic06.png",
       "topics/topic06"
     ),
     new Topic(
       "Isogonal Conjugate and Isotomic Conjugate Points",
       ["Zhiqin Lu", "Shiyi Lyu"],
+      ["Melissa Yu"],
       "public/images/topic07.png",
       "topics/topic07",
       ["isotomic.js"]
@@ -62,30 +77,41 @@
     new Topic(
       "Gergonne and Nagel Points",
       [],
+      [],
       "public/images/topic08.png",
       "topics/topic08"
     ),
     new Topic(
       "Napoleon's Theorem",
       ["Zhiqin Lu"],
+      [],
       "public/images/topic09.png",
       "topics/topic09"
     ),
     new Topic(
       "Ptolemy Theorem's and Kelvin Transform",
       ["Zhiqin Lu", "Felix Yu", "Xianfu Liu"],
+      [],
       "public/images/topic10.png",
       "topics/topic10"
     ),
     new Topic(
       "Dual Triangles",
       ["Zhiqin Lu"],
+      [],
       "public/images/topic11.png",
       "topics/topic11"
     ),
-    new Topic("Euler Line", [], "public/images/topic12.png", "topics/topic12"),
+    new Topic(
+      "Euler Line",
+      [],
+      [],
+      "public/images/topic12.png",
+      "topics/topic12"
+    ),
     new Topic(
       "Nine-point Circle",
+      [],
       [],
       "public/images/topic13.png",
       "topics/topic13"
@@ -93,11 +119,13 @@
     new Topic(
       "Fermat Point",
       [],
+      [],
       "public/images/topic14.png",
       "topics/topic14"
     ),
     new Topic(
       "Complete Quadrangle and Complete Qudrilateral",
+      [],
       [],
       "public/images/topic15.png",
       "topics/topic15"
@@ -105,17 +133,20 @@
     new Topic(
       "Symmedian Point",
       [],
+      [],
       "public/images/topic16.png",
       "topics/topic16"
     ),
     new Topic(
       "Lemoine Circles",
       ["Zhiqin Lu"],
+      [],
       "public/images/topic17.png",
       "topics/topic17"
     ),
     new Topic(
       "Lemoine Line",
+      [],
       [],
       "public/images/topic18.png",
       "topics/topic18"
@@ -123,17 +154,20 @@
     new Topic(
       "Franago Triangle",
       [],
+      [],
       "public/images/topic19.png",
       "topics/topic19"
     ),
     new Topic(
       "Miquel Point",
-      ["Zhiqin Lu", "Shiyi Lyu"],
+      [],
+      [],
       "public/images/topic20.png",
       "topics/topic20"
     ),
     new Topic(
       "Maclaurin's Theorem",
+      [],
       [],
       "public/images/topic21.png",
       "topics/topic21"
@@ -141,11 +175,13 @@
     new Topic(
       "Spieker's Theorem",
       [],
+      [],
       "public/images/topic22.png",
       "topics/topic22"
     ),
     new Topic(
       "Fuhrmann's Theorem",
+      [],
       [],
       "public/images/topic23.png",
       "topics/topic23"
@@ -153,18 +189,21 @@
     new Topic(
       "Harmonic Quadrilateral",
       [],
+      [],
       "public/images/topic24.png",
       "topics/topic24"
     ),
     new Topic(
       "Brocard Points",
       [],
+      [],
       "public/images/topic25.png",
       "topics/topic25"
     ),
     new Topic(
       "Newton Line",
-      [],
+      ["Zhiqin Lu"],
+      ["Mai Nguyen"],
       "public/images/topic26.png",
       "topics/topic26",
       ["newton-line.js"]
@@ -172,11 +211,13 @@
     new Topic(
       "Brahmagupta's Theorem",
       ["Satyaki Sarkar"],
+      [],
       "public/images/topic27.png",
       "topics/topic27"
     ),
     new Topic(
       "Davis' Theorem",
+      [],
       [],
       "public/images/topic28.png",
       "topics/topic28"
@@ -184,6 +225,7 @@
     new Topic(
       "Tucker Circles",
       [],
+      ["Zhiqin Lu"],
       "public/images/topic29.png",
       "topics/topic29",
       ["tucker.js"]
@@ -191,23 +233,27 @@
     new Topic(
       "Taylor Circle",
       ["Jimena Isabel Razo"],
+      [],
       "public/images/topic30.png",
       "topics/topic30"
     ),
     new Topic(
       "Quadrilateral Area Formulas",
       ["Zhiqin Lu"],
+      [],
       "public/images/topic31.png",
       "topics/topic31"
     ),
     new Topic(
       "Monge's Theorem",
       ["Mai Nguyen"],
+      [],
       "public/images/topic32.png",
       "topics/topic32"
     ),
     new Topic(
       "Isodynamic Point",
+      [],
       [],
       "public/images/topic33.png",
       "topics/topic33"
@@ -215,17 +261,20 @@
     new Topic(
       "Viviani' Theorem",
       ["Yufei Ren"],
+      [],
       "public/images/topic34.png",
       "topics/topic34"
     ),
     new Topic(
       "Pappus' Area Theorem",
       ["Dawei Liu"],
+      [],
       "public/images/topic35.png",
       "topics/topic35"
     ),
     new Topic(
       "Projective Harmonic Conjugate",
+      [],
       [],
       "public/images/topic36.png",
       "topics/topic36"
@@ -233,23 +282,27 @@
     new Topic(
       "Trilinear Coordinate System",
       ["Natasha Xiao"],
+      [],
       "public/images/topic37.png",
       "topics/topic37"
     ),
     new Topic(
       "Routh's Theorem",
       ["Yue Chu"],
+      [],
       "public/images/topic38.png",
       "topics/topic38"
     ),
     new Topic(
       "Monge's Problem",
       ["Mai Nguyen"],
+      [],
       "public/images/topic39.png",
       "topics/topic39"
     ),
     new Topic(
       "Steiner-Lehmus' Theorem",
+      [],
       [],
       "public/images/topic40.png",
       "topics/topic40"
@@ -330,7 +383,6 @@
   const navBackToTop = document.getElementById("back-to-top-wrapper");
   nav.addEventListener("scroll", () => {
     const offset = header.clientHeight + nav.clientHeight - window.innerHeight;
-    console.log(offset);
     const y = 12 + offset;
     if (nav.scrollTop >= 20) {
       navBackToTop.style.opacity = "100%";
