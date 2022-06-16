7b761de50527b2caa9120f06a968d0cf055d21fb
Lazurite259 <hsiavivi@gmail.com>
2022-06-15 19:53:59 -0700

    feat(topics): added topic 17-24 view files

---
+++ src/views/topics/GdpTopic22View.vue
@@ -0,0 +1,22 @@
+<template>
+    <TopicMeta :topic="topic" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { indexTopicMap } from '@/data';
+import { Topic } from '@/types';
+
+const topic = indexTopicMap.get(22) as Topic;
+
+export default defineComponent(
+    {
+        setup() {
+            return { topic };
+        },
+        mounted() {
+
+        }
+    },
+);
+</script>
