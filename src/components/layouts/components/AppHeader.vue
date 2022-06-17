7c51ac5e2b457d309e3c976e31f93cd30828b49a
Xinxi Guo <44930252+Kiyo5hi@users.noreply.github.com>
2022-06-17 10:27:51 +0800

    fix(sider): fixed selectedKeys in sider

--- src/components/layouts/components/AppHeader.vue
+++ src/components/layouts/components/AppHeader.vue
@@ -1,13 +1,15 @@
 <template>
-  <ALayoutHeader>
-    <ASpace>
-      <GdpLogo :width="36" :height="36" :preview="false" />
-      <span :style="{
-        color: '#fff',
-        fontSize: '18px',
-      }">Geometry Discovery Project @ UCI</span>
-    </ASpace>
-  </ALayoutHeader>
+  <RouterLink to="/">
+    <ALayoutHeader>
+      <ASpace>
+        <GdpLogo :width="36" :height="36" :preview="false" />
+        <span :style="{
+          color: '#fff',
+          fontSize: '18px',
+        }">Geometry Discovery Project @ UCI</span>
+      </ASpace>
+    </ALayoutHeader>
+  </RouterLink>
 </template>
 
 <script setup lang="ts">
