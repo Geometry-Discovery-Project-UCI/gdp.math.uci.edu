323209039d7c486a85ec334689e0560de2830952
YI CHEN <35242621+Cheryl0402@users.noreply.github.com>
2022-06-19 07:16:34 -0700

    feat(topics): added topic 2-8 view.vue and router paths (#100)
    Added topic 2-8 view.vue and router paths

---
+++ src/views/topics/GdpTopic08View.vue
@@ -0,0 +1,17 @@
+<template>
+  <TopicMeta :topic="topic" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { indexTopicMap } from '@/data';
+import { Topic } from '@/types';
+const topic = indexTopicMap.get(8) as Topic;
+export default defineComponent(
+  {
+    setup() {
+      return { topic };
+    },
+  },
+);
+</script>
