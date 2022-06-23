fbb0f904abe74cf6e657f7e2d1c7674bf7d8dd6f
Jimmykoki <jianlil2@uci.edu>
2022-06-24 01:38:41 +0800

    merge updated contents from main rep

--- src/utils/geoutils.js
+++ src/utils/geoutils.js
@@ -46,12 +46,9 @@ export const area = (x1, y1, x2, y2, x3, y3) => {
 export const distToLine = (p, v, w) => {
   const l2 = findDistance(v, w);
   if (l2 === 0) return findDistance(p, v);
-  const t =
-    ((p[0] - v[0]) * (w[0] - v[0]) + (p[1] - v[1]) * (w[1] - v[1])) / l2;
+  const t = ((p[0] - v[0]) * (w[0] - v[0]) + (p[1] - v[1]) * (w[1] - v[1])) / l2;
 
-  return Math.sqrt(
-    findDistance(p, [v[0] + t * (w[0] - v[0]), v[1] + t * (w[1] - v[1])])
-  );
+  return Math.sqrt(findDistance(p, [v[0] + t * (w[0] - v[0]), v[1] + t * (w[1] - v[1])]));
 };
 
 export const pointAlongLine = (A, B, d) => {
