f28168e91bb4623c7d58d7537102651e6e7e320c
Ish-D <Ishan.Darji0@gmail.com>
2022-07-16 19:33:01 -0700

    Add numbers to animation

--- src/views/topics/GdpTopic34View.vue
+++ src/views/topics/GdpTopic34View.vue
@@ -30,6 +30,7 @@ export default defineComponent(
             backgroundColor: "floralwhite",
         }); 
 
+        // create all lines/labels
         var angleD = new fabric.Rect({width: 10, height: 10, fill: '', stroke: 'black'});
         var angleF = new fabric.Rect({width: 10, height: 10, fill: '', stroke: 'black', angle: -60});
         var angleE = new fabric.Rect({width: 10, height: 10, fill: '', stroke: 'black', angle: 60});
@@ -56,7 +57,11 @@ export default defineComponent(
         const eLabel = makeLabel("E");
         const dLabel = makeLabel("D");
 
-        const equation = makeLabel("PF + PE + PD = H");
+        const valuePD = makeLabel("0");
+        const valuePF = makeLabel("0");
+        const valuePE = makeLabel("0");
+
+        const equation = makeLabel("PD + PE + PF = H");
 
         // create triangle
         const triangle = new fabric.Polygon(
@@ -119,7 +124,6 @@ export default defineComponent(
             // calculate location of points E/F from point of intersection between long lines PF/AB and PE/AC
             const lineProjectPF = solveLinearEquation({x:projectPF.x1, y:projectPF.y1}, {x:projectPF.x2,y:projectPF.y2});
             const pointF = calculateLineIntersectInLinearEquation(lineAB.m, lineAB.b, lineProjectPF.m, lineProjectPF.b);
-
             const lineProjectPE = solveLinearEquation({x:projectPE.x1, y:projectPE.y1}, {x:projectPE.x2,y:projectPE.y2});
             const pointE = calculateLineIntersectInLinearEquation(lineAC.m, lineAC.b, lineProjectPE.m, lineProjectPE.b);
 
@@ -153,106 +157,65 @@ export default defineComponent(
             const peDist = calculateDistanceBetweenTwoPoints({x:pointP.left, y:pointP.top}, {x:pointE.x, y:pointE.y});
             const pfDist = calculateDistanceBetweenTwoPoints({x:pointP.left, y:pointP.top}, {x:pointF.x, y:pointF.y});
 
+            // draw values of individual side lengths
+            const sumDist = pdDist + peDist + pfDist;
+            valuePD.set({text: (pdDist/sumDist).toFixed(3).toString() + " +", left:110, top:90});
+            valuePE.set({text: (peDist/sumDist).toFixed(3).toString() + " +", left:190, top:90});
+            valuePF.set({text: (pfDist/sumDist).toFixed(3).toString() + " = 1", left:270, top:90});
+
             // Draw side lines based off length of PE/PD/PF
             sumPD.set({
-                x1: 425,
+                x1: points[0].x,
                 y1: 150,
-                x2: 425,
+                x2: points[0].x,
                 y2: 150 + pdDist,
                 stroke: "red",
                 strokeWidth: 2,
             });
 
             sumPE.set({
-                x1: 425,
+                x1: points[0].x,
                 y1: 150 + pdDist,
-                x2: 425,
+                x2: points[0].x,
                 y2: 150 + pdDist + peDist,
                 stroke: "green",
                 strokeWidth: 2,
             });            
 
             sumPF.set({
-                x1: 425,
+                x1: points[0].x,
                 y1: 150 + pdDist + peDist,
-                x2: 425,
+                x2: points[0].x,
                 y2: 150 + pdDist + peDist + pfDist,
                 stroke: "blue",
                 strokeWidth: 2,
             });  
 
             lineHeight.set({
-                x1: points[0].x,
-                y1: points[0].y,
-                x2: points[0].x,
+                x1: 425,
+                y1: 150,
+                x2: 425,
                 y2: 450,
                 stroke: "black",
+                strokeWidth: 2,
             });
 
             // set coordinates for the labels
-            aLabel.set({
-            left: points[0].x - 5,
-            top: points[0].y - 30,
-            fontSize: 20,
-            });
+            aLabel.set({left: points[0].x - 5, top: points[0].y - 30, fontSize: 20});
+            bLabel.set({left: points[1].x - 10, top: points[1].y + 10, fontSize: 20});
+            cLabel.set({left: points[2].x + 10, top: points[2].y + 10, fontSize: 20});
 
-            bLabel.set({
-            left: points[1].x - 10,
-            top: points[1].y + 10,
-            fontSize: 20,
-            });
+            dLabel.set({left: pointP.left as number, top: 450, fontSize: 18});
+            eLabel.set({left: pointE.x - 20, top: pointE.y - 15, fontSize: 18});
+            fLabel.set({left: pointF.x - 20,top: pointF.y - 15,fontSize: 18});
 
-            cLabel.set({
-            left: points[2].x + 10,
-            top: points[2].y + 10,
-            fontSize: 20,
-            });
-
-            dLabel.set({
-            left: pointP.left as number,
-            top: 450,
-            fontSize: 18,
-            });
-
-            eLabel.set({
-            left: pointE.x - 20,
-            top: pointE.y - 15,
-            fontSize: 18,
-            });
-
-            fLabel.set({
-            left: pointF.x - 20,
-            top: pointF.y - 15,
-            fontSize: 18,
-            });
-
-            hLabel.set({
-            left: 210,
-            top: 300,
-            fontSize: 18,
-            });
-
-            pLabel.set({
-            left: pointP.left as number - 25,
-            top: pointP.top as number - 10,
-            fontSize: 18,
-            });
+            hLabel.set({left: 430,top: 300,fontSize: 18});
+            pLabel.set({left: pointP.left as number - 25,top: pointP.top as number - 10,fontSize: 18});
             
             // Align squares to their respective points
-            angleD.set({
-            left: pointP.left as number - 10,
-            top: 440,
-            });
-
-            angleF.set({
-            left: pointF.x,
-            top: pointF.y,
-            });
-
-            angleE.set({
-            left: pointE.x,
-            top: pointE.y,
-            });
+            angleD.set({left: pointP.left as number - 10,top: 440});
+            angleF.set({left: pointF.x, top: pointF.y});
+            angleE.set({left: pointE.x, top: pointE.y});
         }
 
         equation.set({left:135, top:50, fontSize:30});
@@ -261,6 +224,7 @@ export default defineComponent(
 
         canvas.on("object:moving", movePointP);
 
+        // Draw all objects
         canvas.add(triangle);
 
         canvas.add(linePD);
@@ -285,6 +249,10 @@ export default defineComponent(
         canvas.add(angleD);
         canvas.add(angleE);
         canvas.add(angleF);
+
+        canvas.add(valuePD);
+        canvas.add(valuePE);
+        canvas.add(valuePF);
 
         canvas.add(pLabel);
         canvas.add(pointP);
